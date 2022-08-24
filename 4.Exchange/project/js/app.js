ec05a86ea71d9633c12fe4c90529b83441d810ca
waiyanheinhero <waiyanhein.hero@gmail.com>
2022-08-24 16:01:34 +0630

    update-9

--- 4.Exchange/project/js/app.js
+++ 4.Exchange/project/js/app.js
@@ -1,7 +1,10 @@
 let inputs = document.getElementById("input")
-let from = document.getElementById("from"); //from select ဖမ်းတာ
-let to = document.getElementById("to"); // to select
-let result = document.querySelector(".result")
+let from = document.getElementById("from");             //from select ဖမ်းတာ
+let to = document.getElementById("to");                 // to select
+let result = document.querySelector(".result");
+let calc = document.getElementById("calc");
+let historyList = document.getElementById("history-list")
+
 
 function createOption(x,y,z){                    // y = data.rate  ထဲက  အရှေ့ခြမ်းကိုယူထားတာ                         
     let o = document.createElement("option");
@@ -21,7 +24,7 @@ for (x in data.rates){
     // console.log(x+"--"+data.rates[x]);
 }
 
-document.getElementById("calc").addEventListener("submit",function (event){
+calc.addEventListener("submit",function(event){
     event.preventDefault();
     //get state
     let x = inputs.value;
@@ -29,20 +32,62 @@ document.getElementById("calc").addEventListener("submit",function (event){
     let z = to.value;
     console.log(x,y,z);
 
+
     //process
     let first = x * y;
     console.log(first);
     let second = first/z;
-    console.log(second.toFixed(2));
+    // console.log(second.toFixed(2));
+    let fromText = x +" - "+ from.options[from.selectedIndex].innerText;
+    let toText = to.options[to.selectedIndex].innerText;
+    let results = second.toFixed(2)+" - "+to.options[to.selectedIndex].innerText;
+    // console.log(fromText,toText);
+    let d = new Date().toLocaleString();
+    let arr = [d,fromText,toText,results];
+    history(arr);
+    store()
+    
+
 
     //set state
-    result.innerHTML = second.toFixed(2);
+    result.innerHTML = results;
+    
 
     inputs.value = "";
     inputs.focus();
     from.value = "";
-    to.value = "1";
-})
+    to.value = "1";  
+});
+
+
+// function test(){
+//     console.log(from.options[from.selectedIndex].innerText)    //option တွေထဲက arr number တစ်ခုချင်းစီကိုဖမ်းတာ
+// }
+
+function history(x){
+
+    let tr = document.createElement("tr");
+
+    x.map(function(el){
+        let td = document.createElement("td");
+        let tdText = document.createTextNode(el);
+        td.appendChild(tdText)
+        tr.appendChild(td);
+        // console.log(tr);
+    })
+
+    historyList.append(tr)
+}
+
+(function(){
+    if(localStorage.getItem("record")){
+        historyList.innerHTML = localStorage.getItem("record");
+    }
+})()
+   
+function store(){
+    localStorage.setItem("record",historyList.innerHTML)
+}
 
 
 
