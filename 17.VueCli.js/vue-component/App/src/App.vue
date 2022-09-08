2c8ed775f45408d9102bc1596b66086d9249cf1c
waiyanheinhero <waiyanhein.hero@gmail.com>
2022-09-08 16:09:54 +0630

    vuecli2

---
+++ 17.VueCli.js/vue-component/App/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="p-3">
+      <Title/>
+      <ul class="list-group">
+        <List></List>
+        <List></List>
+      </ul>
+  </div>
+</template>
+
+<script>
+  import Title from "./components/Title.vue";
+  import List from "./components/List.vue";
+  export default {
+      components: {
+         Title,
+         List
+      }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
