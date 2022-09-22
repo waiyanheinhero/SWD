ace1c7121693af3baf5c240cc047e319bdbd5474
waiyanheinhero <waiyanhein.hero@gmail.com>
2022-09-22 13:59:37 +0630

    dish

--- 8.AdminDashboard/project/project/js/dashboard.js
+++ 8.AdminDashboard/project/project/js/dashboard.js
@@ -69,3 +69,58 @@ let ovChart = new Chart(ov, {
         }
     }
 });
+
+let orderFromPlace = [5,15,4,9,7];
+let places = ["YGN","MDY","NPY","SHAN","MGW"];
+
+let op = document.getElementById('op').getContext('2d');
+let opChart = new Chart(op, {
+    type: 'doughnut',
+    data: {
+        labels:places,
+        datasets: [{
+            label: '# of Votes',
+            data:orderFromPlace,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                display:false,
+                ticks: {
+                    beginAtZero: true
+                }
+            }],
+            xAxes: [
+                {
+                    display:false
+                }
+            ]
+        },
+        legend:{
+            display: true,
+            position: 'bottom',
+            labels: {
+                fontColor: '#333',
+                usePointStyle:true
+            }
+        }
+    }
+});
